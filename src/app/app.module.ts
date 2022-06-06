bf5b345e5587ce53f423500f03bc0577d1babdcc
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-06-06 18:17:07 +0200

    Quito PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule }           from '@angular/platform-browser';
 import { NgModule }                from '@angular/core';
-import { ServiceWorkerModule }     from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule }             from '@angular/forms';
 import { AppRoutingModule }        from './app-routing.module';
 import { AppComponent }            from './app.component';
-import { environment }             from '../environments/environment';
 
 import { TokenInterceptor }        from './interceptors/token.interceptor';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -27,7 +25,6 @@ const appearance: MatFormFieldDefaultOptions = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
