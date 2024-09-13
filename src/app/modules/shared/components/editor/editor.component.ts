d90e8fd2c8ac4ba0885a9dd4fee6f0d36784b6c4
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-09-13 10:34:28 +0200

    Faltaba importar una clase para estilos de botones

--- src/app/modules/shared/components/editor/editor.component.ts
+++ src/app/modules/shared/components/editor/editor.component.ts
@@ -10,6 +10,7 @@ import {
   viewChild,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -46,6 +47,7 @@ import { firstValueFrom } from 'rxjs';
     MatIconModule,
     MatSlideToggleModule,
     MatCardModule,
+    MatButton,
   ],
   providers: [DialogService],
 })
